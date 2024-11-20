f8c5a3bfbf0980331b2a054849058f9030dae1f0
Itokiana <itokianarajoelison@gmail.com>
2024-11-20 11:23:48 +0300

    📦 NEW: create basic register user

--- Data/ApplicationDBContext.cs
+++ Data/ApplicationDBContext.cs
@@ -1,4 +1,5 @@
 using beer_app_management.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
@@ -14,5 +15,29 @@ namespace api.Data
 
         public DbSet<Beer> Beer { get; set; }
         public DbSet<WSStock> WSStock { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            List<IdentityRole> roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Name = "Admin",
+                    NormalizedName = "ADMIN",
+                },
+                new IdentityRole
+                {
+                    Name = "Brewer",
+                    NormalizedName = "BREWER",
+                },
+                new IdentityRole
+                {
+                    Name = "Wholesaler",
+                    NormalizedName = "WHOLESALER",
+                },
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
     }
 }
---
+++ Migrations/20241120081557_SeedRole.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace beer_app_management.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "113f54dd-6884-48d6-977d-abd774242a23", null, "Brewer", "BREWER" },
+                    { "8c65408b-637f-48e6-a84e-624fe96e4013", null, "Admin", "ADMIN" },
+                    { "e39c7410-fd4a-437f-bf04-929c9d5609ef", null, "Wholesaler", "WHOLESALER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "113f54dd-6884-48d6-977d-abd774242a23");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "8c65408b-637f-48e6-a84e-624fe96e4013");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "e39c7410-fd4a-437f-bf04-929c9d5609ef");
+        }
+    }
+}
